db5d50904dab84d92ae10bd0c5db6c9b6a116a59
alexxyu <yu.alexanderx@gmail.com>
2020-02-01 14:43:13 -0800

    create new footstep endpoint

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -46,7 +46,44 @@ const UserController = (UserModel) =>
         return res.status(404).json({
             error: 'Invalid User ID'
         });
-    })
+    });
+
+    router.post('/footstep', async(req, res) => {
+
+        // Validate request body
+        const fb_id = req.body.fb_id;
+        if (fb_id === undefined)
+            return res.status(400).json({
+                error: 'Malformed request'
+            });
+
+        // Check if user exists
+        const priorData = await UserModel.findOne({fb_id: fb_id});
+        if (priorData == null) 
+            return res.status(404).json({
+                error: 'Invalid User ID'
+            });
+
+        // Create and insert new footstep
+        const new_footstep = { date: new Date(), 
+                               foodLog: [], 
+                               transportationLog: [], 
+                               stats: {
+                                   emissions: 0,
+                                   waterUsed: 0,
+                                   distanceWalked: 0,
+                                   distanceByCar: 0
+                               } };
+
+        let data = await UserModel.update(
+            { fb_id: fb_id },
+            { $push: { footsteps: new_footstep } },
+            { new: true }
+        );
+
+        return res.status(200).json(data);
+
+    });
 
     return router;
 }
